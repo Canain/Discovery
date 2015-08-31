a19ec471532ce9e63c6654aa0e7f1993456873a0
Shuyang Chen <sc1085@gmail.com>
2015-08-31 18:59:47 -0400

    Added get current date

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -3,7 +3,14 @@
 import Graph = require('./graph');
 
 $(document).ready(() => {
-	$.getJSON('/api/topics/filter?day=2015-08-27', (data, textStatus: string, jqXHR: JQueryXHR) => {
+	var today = new Date();
+	var month = (today.getMonth() + 1).toString();
+	if (month.length < 2) {
+		month = '0' + month;
+	}
+	var date = today.getFullYear() + '-' + month + '-' + today.getDate();
+	document.title = document.title + ' ' + date;
+	$.getJSON('/api/topics/filter?day=' + date, (data, textStatus: string, jqXHR: JQueryXHR) => {
 		var graph = window['graph'] = new Graph(data[0].graph);
 	});
 });
